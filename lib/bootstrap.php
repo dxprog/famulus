7ab0070dcdaf2176a214ac7b196f7f54dab7cef8
Matt Hackmann <mhackmann@linkedin.com>
2015-02-20 08:52:05 -0800

    tabs to spaces

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -6,51 +6,51 @@
 
 namespace Lib {
 
-	use Api;
-	use Exception;
-	use Lib;
+    use Api;
+    use Exception;
+    use Lib;
 
-	// Check for a composer autoloader and require it if found
-	if (is_readable('./vendor/autoload.php')) {
-		require_once('./vendor/autoload.php');
-	}
+    // Check for a composer autoloader and require it if found
+    if (is_readable('./vendor/autoload.php')) {
+        require_once('./vendor/autoload.php');
+    }
 
-	class Bootstrap {
+    class Bootstrap {
 
-		private static $_initialized = false;
+        private static $_initialized = false;
 
-		public static function initialize() {
-			if (!self::$_initialized) {
-				spl_autoload_register('Lib\\Bootstrap::classLoader');
-				self::$_initialized = true;
-			}
-		}
+        public static function initialize() {
+            if (!self::$_initialized) {
+                spl_autoload_register('Lib\\Bootstrap::classLoader');
+                self::$_initialized = true;
+            }
+        }
 
-		/**
-		 * Class auto loader
-		 */
-		private static function classLoader($library) {
+        /**
+         * Class auto loader
+         */
+        private static function classLoader($library) {
 
-			$library = explode('\\', $library);
+            $library = explode('\\', $library);
 
-			// Pop off the actual class name to preserve casing
-			$fileName = array_pop($library);
+            // Pop off the actual class name to preserve casing
+            $fileName = array_pop($library);
 
-			// Find the path to the namespace
-			$filePath = '.';
-			foreach ($library as $namespace) {
-				$filePath .= '/' . strtolower($namespace);
-			}
+            // Find the path to the namespace
+            $filePath = '.';
+            foreach ($library as $namespace) {
+                $filePath .= '/' . strtolower($namespace);
+            }
 
-			$filePath .= '/' . $fileName . '.php';
-			if (is_readable($filePath)) {
-				require_once($filePath);
-			}
+            $filePath .= '/' . $fileName . '.php';
+            if (is_readable($filePath)) {
+                require_once($filePath);
+            }
 
-		}
+        }
 
-	}
+    }
 
-	Bootstrap::initialize();
+    Bootstrap::initialize();
 
 }
