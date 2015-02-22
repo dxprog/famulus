4ed2ab7d6f854d55d71590db4b612cb8ed7ae515
Matt Hackmann <matt@dxprog.com>
2015-02-21 22:25:43 -0800

    test db output updated to match Db

--- test/harness/test_db.php
+++ test/harness/test_db.php
@@ -3,28 +3,32 @@
 // Test database lib
 
 namespace Lib {
-    
+
     use stdClass;
 
     class Db {
 
-        public static $lastResult;
+        private static $_queries = [];
 
-        public static function Connect() {
-
-        }
+        public static function Connect() { } // NOOP
 
         public static function Query($query, $params = null) {
-            
-            $retVal = new stdClass;
-            $retVal->query = $query;
-            $retVal->params = $params;
-            self::$lastResult = $retVal;
+
+            $retVal = (object)[
+                'query' => $query,
+                'params' => $params
+            ];
+
+            self::$_queries[] = $retVal;
 
             // Action specific returns
             switch (strtolower(current(explode(' ', $query)))) {
+                case 'select':
+                    $retVal->count = 0;
+                    break;
                 case 'insert':
-                    $retVal = 1;
+                    $retVal->insertId = 1;
+                    $retVal->count = 1;
                     break;
                 case 'update':
                 case 'delete':
@@ -37,6 +41,10 @@ namespace Lib {
 
         public static function Fetch($resource) {
             return $resource;
+        }
+
+        public static function getLastResult() {
+            return end(self::$_queries);
         }
 
     }
